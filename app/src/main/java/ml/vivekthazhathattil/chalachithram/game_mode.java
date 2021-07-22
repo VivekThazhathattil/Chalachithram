9e46e93220d117e56d510e4e1253b24da7fbe099
Vivek Thazhathattil <vivek.thazhathattil@gmail.com>
2021-07-22 22:27:10 +0530

    fix issue #3 and #4

--- app/src/main/java/ml/vivekthazhathattil/chalachithram/game_mode.java
+++ app/src/main/java/ml/vivekthazhathattil/chalachithram/game_mode.java
@@ -20,12 +20,6 @@ import android.widget.Toast;
 
 import com.example.malayalamdialoguesquiz.R;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link game_mode#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class game_mode extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -47,14 +41,6 @@ public class game_mode extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment game_mode.
-     */
     // TODO: Rename and change types and number of parameters
     public static game_mode newInstance(String param1, String param2) {
         game_mode fragment = new game_mode();
